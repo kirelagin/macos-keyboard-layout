cc6c0421b7b5bc95a7c8fc445477212d5953d490
Kirill Elagin <kirelagin@gmail.com>
2019-06-25 16:20:53 +0300

    Reorganise code to be more pythonic
    The code used to be not flexible enough to proceed with further improvements.
    
    Introduce a couple of classes, make it all more modular and more
    structured so that it is possible to generate more advanced layouts.

--- src/layout.py
+++ src/layout.py
@@ -1,54 +1,72 @@
 # SPDX-License-Identifier: MPL-2.0
 # © 2019 Kirill Elagin <kirelagin@gmail.com>
 
+from collections import OrderedDict
 import re
 import xml.etree.cElementTree as ET
 
-from layer import combine_layers, resolve_map
+from key_map import IdMap
 from layers import english, typo
 from maps import other, special
 
 
-doctype = '<!DOCTYPE keyboard SYSTEM "file://localhost/System/Library/DTDs/KeyboardLayout.dtd">'
+class Layout:
+  """A keyboard layout consisting of multiple levels."""
+
+  def __init__(self, group, id, name, levels):
+    """Create a new keyboard layout.
+
+    levels: ordered map from modifiers list to IdMap
+    """
+    self._group = group
+    self._id = id
+    self._name = name
+    self._levels = levels
+
+  # XML
+  def yield_xml(self):
+    yield '<?xml version="1.1" encoding="UTF-8"?>'
+    yield '<!DOCTYPE keyboard SYSTEM "file://localhost/System/Library/DTDs/KeyboardLayout.dtd">'
+    yield '<keyboard group="{}" id="{}" name="{}" maxout="1">'.format(self._group, self._id, self._name)
+
+    def yield_layouts():
+      yield '<layouts>'
+      yield '<layout first="0" last="17" mapSet="ANSI" modifiers="Mods"/>'
+      yield '</layouts>'
+    for line in yield_layouts():
+      yield line
+
+    def yield_modifier_map():
+      yield '<modifierMap id="Mods" defaultIndex="0">'
+      for index, mods in enumerate(self._levels.keys()):
+        yield '  <keyMapSelect mapIndex="{}">'.format(index)
+        yield '    <modifier keys="{}"/>'.format(mods)
+        yield '  </keyMapSelect>'
+      yield '</modifierMap>'
+    for line in yield_modifier_map():
+      yield line
+
+    def yield_key_map_sets():
+      yield '<keyMapSet id="ANSI">'
+      for index, id_map in enumerate(self._levels.values()):
+        for line in id_map.yield_xml(index):
+          yield '  ' + line
+      yield '</keyMapSet>'
+    for line in yield_key_map_sets():
+      yield line
+
+    yield '</keyboard>'
 
 # This is a temporary function.
 # TODO: Generalise it.
 def write_english_typo():
-  keyboard = ET.Element("keyboard", group="126", id="-8324", name="English (US) Typo", maxout="1")
+  layers = OrderedDict([
+    ('', IdMap(special.id_char) | IdMap(other.id_char) | IdMap.from_keymap(english.normal)),
+    ('anyShift', IdMap(special.id_char) | IdMap(other.id_char) | IdMap.from_keymap(english.shifted)),
+    ('anyOption', IdMap(special.id_char) | IdMap.from_keymap(typo.normal)),
+    ('anyShift anyOption', IdMap(special.id_char) | IdMap.from_keymap(typo.shifted)),
+  ])
 
-  layouts = ET.SubElement(keyboard, "layouts")
-  layout = ET.SubElement(layouts, "layout", first="0", last="17", mapSet="ANSI", modifiers="Mods")
+  layout = Layout(0, -8324, 'English (US) Typo', layers)
 
-  modMap = ET.SubElement(keyboard, "modifierMap", id="Mods", defaultIndex="0")
-  mod0 = ET.SubElement(modMap, "keyMapSelect", mapIndex="0")
-  ET.SubElement(mod0, "modifier", keys="")
-  mod1 = ET.SubElement(modMap, "keyMapSelect", mapIndex="1")
-  ET.SubElement(mod1, "modifier", keys="anyShift")
-  mod2 = ET.SubElement(modMap, "keyMapSelect", mapIndex="2")
-  ET.SubElement(mod2, "modifier", keys="anyOption")
-  mod3 = ET.SubElement(modMap, "keyMapSelect", mapIndex="3")
-  ET.SubElement(mod3, "modifier", keys="anyShift anyOption")
-
-  kms = ET.SubElement(keyboard, "keyMapSet", id="ANSI")
-
-  write_map(ET.SubElement(kms, "keyMap", index="0"), combine_layers(special.id_char, other.id_char, resolve_map(english.normal)))
-  write_map(ET.SubElement(kms, "keyMap", index="1"), combine_layers(special.id_char, other.id_char, resolve_map(english.shifted)))
-  write_map(ET.SubElement(kms, "keyMap", index="2"), combine_layers(special.id_char, resolve_map(typo.normal)))
-  write_map(ET.SubElement(kms, "keyMap", index="3"), combine_layers(special.id_char, resolve_map(typo.shifted)))
-
-  tree = ET.ElementTree(keyboard)
-  print('<?xml version="1.1" encoding="UTF-8"?>')
-  print(doctype)
-
-  s = ET.tostring(keyboard, encoding='unicode')
-  #s = re.sub(r'"\\x(..)"', r'"&#x\1;"', s)
-  s = re.sub(r'"&#10;"', r'"&#x0D;"', s)  # TODO: wtf?
-  s = re.sub(r'"&#09;"', r'"&#x09;"', s)  # TODO: wtf?
-  #s = re.sub(r'"&[a-z]*;"', r'"X"', s)
-  s = re.sub(r'><', '>\n<', s)
-  print(s)
-
-
-def write_map(el, layer):
-  for k, v in layer.items():
-    ET.SubElement(el, "key", code=str(k), output=v)
+  print('\n'.join(layout.yield_xml()))
